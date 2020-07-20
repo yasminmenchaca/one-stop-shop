5c6c30c2ce4fb306cb2dbdc595abc4e0520fcfd7
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-07-20 10:12:08 -0500

    testing for AWS deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello from One Stop Shop");
+    res.send("Updated- Hello from One Stop Shop");
 });
 
 const port = process.env.port || 3000;
