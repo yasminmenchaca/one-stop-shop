853985b705496d16786a409246bbab2d08167f13
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-07-20 09:43:42 -0500

    updated port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.send("Hello from One Stop Shop");
 });
 
-const port = process.env.port || 8080;
+const port = process.env.port || 3000;
 
 app.listen(port, () => {
     console.log("Hi");
